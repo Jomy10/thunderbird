2d38b690608233cf0e3e0e29359dcf2d68293f6c
Jomy10 <jonas.vbs4@gmail.com>
2022-09-20 18:15:49 +0200

    added shell option to build-other script

--- thunderbird-emulator/build-other.sh
+++ thunderbird-emulator/build-other.sh
@@ -13,18 +13,27 @@ do
   
     if [[ "$var" == "--pass-c-example" ]]; then
       PREV_ARG="c"
+    elif [[ "$var" == "--sh" ]]
+      PREV_ARG="sh"
     fi
+    
   elif [[ "$PREV_ARG" == "c" ]]; then
     C_ARGS+=$var
     C_ARGS+=" "
+    PREV_ARG="nil"
+  elif [[ "$PREV_ARG" == "sh" ]]; then
+    sh=$var
+    PREV_ARG="nil"
   fi
 
 done
 
+SH=sh
+
 # Build examples
-sh ../examples/rust/space-invader/build.sh
-sh ../examples/wasm/drawing-app/build.sh
-sh ../examples/c/music/build.sh $C_ARGS
+$SH ../examples/rust/space-invader/build.sh
+$SH ../examples/wasm/drawing-app/build.sh
+$SH ../examples/c/music/build.sh $C_ARGS
 
 # Build wasm
 node build-wasm.js
