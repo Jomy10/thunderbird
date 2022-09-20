f56b305c32ddd86308c91cf10b6a92e226bd8a14
Jomy10 <jonas.vbs4@gmail.com>
2022-09-20 18:00:32 +0200

    modified c music example build script

--- examples/c/music/build.sh
+++ examples/c/music/build.sh
@@ -1,15 +1,44 @@
 set -e
 cd "$(dirname "$0")"
 
+CLANG="clang"
+
 SOURCE_FILE="main.c"
 OUT_FILE="../../../thunderbird-emulator/public/wasm/sound_game.wasm"
 
 LLVM_PATH=true
 
-if [[ $LLVM_PATH ]]; then
+PREV_ARG="nil"
+
+echo "PREV_ARG=$PREV_ARG"
+
+for var in "$@"
+do
+
+  if [[ "$PREV_ARG" == "nil" ]]; then
+  
+    if [[ "$var" == "-no-llvm-path" ]]; then
+      LLVM_PATH=false
+    elif [[ "$var" == --clang ]]; then
+      PREV_ARG="clang"
+    fi
+    
+  elif [[ "$PREV_ARG" == "clang" ]]; then
+    
+    CLANG="clang$var"
+    PREV_ARG="nil"
+    
+  else
+    echo "unknown argument: $var"
+  fi
+done
+
+echo $CLANG
+
+if [[ "$LLVM_PATH" == "true" ]]; then
   
   PATH="/usr/local/opt/llvm/bin:$PATH" \
-  clang $SOURCE_FILE \
+  $CLANG $SOURCE_FILE \
     --target=wasm32 \
     -nostdlib \
     -O2 \
@@ -24,7 +53,9 @@ if [[ $LLVM_PATH ]]; then
   
 else
 
-  clang $SOURCE_FILE \
+  echo "Running without specified llvm path"
+
+  $CLANG $SOURCE_FILE \
     --target=wasm32 \
     -nostdlib \
     -O2 \
